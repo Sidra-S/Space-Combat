3e29faa057ba6512d2553c93f8f461c1b7b9eb1c
Sidra-S <sidsar112@gmail.com>
2022-05-26 13:41:24 +0530

    Game(with score)

--- Game.py
+++ Game.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-
+import math
 pygame.init()
 
 window = pygame.display.set_mode((800, 600))
@@ -24,16 +24,22 @@ def display_ship(x, y):
 
 
 # enemy
-enemy = pygame.image.load("D:\\Sidra\\Decision Modeling\\Project\\Enemy.png")
-enemy = pygame.transform.scale(enemy, (60, 70))
-E_X = random.randint(0, 800)
-E_Y = random.randint(50, 150)
-enemy_variable_pos_x = 3
-enemy_variable_pos_y = 3
+enemy = []
+E_X = []
+E_Y = []
+enemy_variable_pos_x = []
+enemy_variable_pos_y = []
+no_enemy = 7
+for i in range(no_enemy):
+    enemy.append(pygame.image.load("D:\\Sidra\\Decision Modeling\\Project\\Enemy.png"))
+    E_X.append(random.randint(0, 800))
+    E_Y.append(random.randint(50, 150))
+    enemy_variable_pos_x.append(3)
+    enemy_variable_pos_y.append(3)
 
 
-def display_enemy(x, y):
-    window.blit(enemy, (x, y))
+def display_enemy(x, y, i):
+    window.blit(enemy[i], (x, y))
 
 
 # bullet
@@ -41,14 +47,29 @@ bullet = pygame.image.load("D:\\Sidra\\Decision Modeling\\Project\\b2.png")
 bullet = pygame.transform.scale(bullet, (60, 50))
 B_X = 0
 B_Y = 460
-b_variable_pos_y = 8
+b_variable_pos_y = 17
 
 b_state = False
 
 
 def fire_bullets(x, y):
     window.blit(bullet, (x, y))
+#collision
+def collision_detection(E_X,E_Y,B_X,B_Y):
+    dist=math.sqrt(math.pow(E_X-B_X,2)+math.pow(E_Y-B_Y,2))
+    if dist <20:
+        return True
+    else:
+        return False
+#scorring sys
+score=0
+font = pygame.font.Font('freesansbold.ttf',32)
+X = 10
+Y = 10
 
+def display_score(X,Y):
+    s = font.render("Score : "+ str(score),True, (139,0,139))
+    window.blit(s , (X,Y))
 
 run = True
 while run:
@@ -86,17 +107,33 @@ while run:
         B_Y -= b_variable_pos_y
         fire_bullets(B_X + 15, B_Y + 15)
 
+    if B_Y<=0:
+        B_Y=480
+        b_state=False
+
+
     # enemy logic
-    E_X += enemy_variable_pos_x
-    if E_X <= 0:
-        enemy_variable_pos_x = 3
-        E_Y += enemy_variable_pos_y
-    if E_X > 736:
-        enemy_variable_pos_x = -3
-        E_Y += enemy_variable_pos_y
+    for i in range(no_enemy):
+        E_X[i] += enemy_variable_pos_x[i]
+        if collision_detection(E_X[i],E_Y[i],B_X,B_Y):
+            B_Y=480
+            score+=5
+            print(score)
+            b_state=False
 
-    display_enemy(E_X, E_Y)
+            E_X[i]=random.randint(0,735)
+            E_Y[i]=random.randint(50, 150)
 
+
+        #if E_X[i] <= 0:
+            #enemy_variable_pos_x[i] = 4
+            #E_Y[i] += enemy_variable_pos_y[i]
+        #if E_X[i] > 736:
+            #enemy_variable_pos_x[i] = -4
+            #E_Y[i] += enemy_variable_pos_y[i]
+
+        display_enemy(E_X[i], E_Y[i], i)
+    display_score(X,Y)
     pygame.display.update()
 
 pygame.quit()
